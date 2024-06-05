3c7a51b2276df63ce03b2712645d452b4fdd1275
juliia <poshtaklon@gmail.com>
2024-06-05 13:53:28 +0300

    friends

--- trip_app/models.py
+++ trip_app/models.py
@@ -12,8 +12,30 @@ class Trip(models.Model):
     trip_name = models.CharField(max_length=100)
     start_date = models.DateField() 
     end_date = models.DateField()
-    destination = models.CharField(max_length=50)
-    #destination = models.ForeignKey('Destination', on_delete=models.CASCADE)
+    #destination = models.CharField(max_length=50)
+    destination = models.ForeignKey('Destination', on_delete=models.CASCADE)
 
-    # def __str__(self) -> str:
-    #     return f"Trip({self.trip_name}, {self.start_date}, {self.end_date})"
+    def __str__(self) -> str:
+        return f"Trip({self.trip_name}, {self.start_date}, {self.end_date})"
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    destination = models.ForeignKey('Destination', on_delete=models.CASCADE)
+    rating = models.SmallIntegerField()
+
+    def __str__(self) -> str:
+        return f"Review({self.user}, {self.destination}, {self.rating})"
+
+
+class Destination(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return f"Destination({self.name})"
+    
+class Friend(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='friends')
+    friend = models.ForeignKey(User, on_delete=models.CASCADE, related_name='friend_of')
+
+    def __str__(self) -> str:
+        return f"The {self.user} is friend of {self.friend}"
